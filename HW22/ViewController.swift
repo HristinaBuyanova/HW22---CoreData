6357688fc9c1836ff830ff8666dc690fcb2b044e
HristinaBuyanova <hristina-a@mail.ru>
2024-02-01 14:52:57 +0300

    Initial Commit

---
+++ HW22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW22
+//
+//  Created by Христина Буянова on 01.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
