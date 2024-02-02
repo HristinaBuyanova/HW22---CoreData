98a18a61dea91e98f7e048c3589476d212d96f0f
HristinaBuyanova <hristina-a@mail.ru>
2024-02-03 00:10:20 +0300

    Create model

---
+++ HW22/Sources/Scenes/Models/User+CoreDataProperties.swift
@@ -0,0 +1,21 @@
+
+
+import Foundation
+import CoreData
+
+
+extension User {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<User> {
+        return NSFetchRequest<User>(entityName: "User")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var gender: String?
+    @NSManaged public var dateOfBirth: Date?
+
+}
+
+extension User : Identifiable {
+
+}
