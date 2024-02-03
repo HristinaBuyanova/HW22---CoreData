481477b02f47690278bf892bfa8b61224a95e52f
HristinaBuyanova <hristina-a@mail.ru>
2024-02-03 15:03:26 +0300

    Create func for core data

--- HW22/Sources/App/AppDelegate.swift
+++ HW22/Sources/App/AppDelegate.swift
@@ -1,29 +1,47 @@
-//
-//  AppDelegate.swift
-//  HW22
-//
-//  Created by Христина Буянова on 01.02.2024.
-//
 
 import UIKit
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    func application(_ application: UIApplication, 
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
 
+    }
+
+    lazy var persistantContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "")
+        container.loadPersistentStores { description, error in
+            if let error {
+                print(error.localizedDescription)
+            } else {
+                print("DB url - ", description.url?.absoluteString)
+            }
+        }
+        return container
+    }()
+
+    func saveContex() {
+        let contex = persistantContainer.viewContext
+        if contex.hasChanges {
+            do {
+                try contex.save()
+            } catch {
+                let myError = error as NSError
+                fatalError(myError.localizedDescription)
+            }
+        }
     }
 
 
---
+++ HW22/Sources/Manager/CoreDaraManager.swift
@@ -0,0 +1,80 @@
+
+
+import UIKit
+import CoreData
+
+public final class CoreDaraManager: NSObject {
+
+    public  static let shared = CoreDaraManager()
+
+    private override init() {}
+
+    private var appDelegate: AppDelegate {
+        UIApplication.shared.delegate as! AppDelegate
+    }
+
+    private var contex: NSManagedObjectContext {
+        appDelegate.persistantContainer.viewContext
+    }
+
+    public func createUser(name: String, phoneNumber: String, dateOfBirth: Date?) {
+        guard let userEntityDescription = NSEntityDescription.entity(forEntityName: "User", in: contex) else { return }
+        let user = User(entity: userEntityDescription, insertInto: contex)
+        user.name = name
+        user.phoneNumber = phoneNumber
+        user.dateOfBirth = dateOfBirth
+
+        appDelegate.saveContex()
+    }
+
+    public func fetchAllUser() -> [User] {
+        do {
+            return try contex.fetch(User.fetchRequest())
+        } catch {
+            print(error.localizedDescription)
+        }
+        return [User()]
+//        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+//        do {
+//            return try contex.fetch(fetchRequest) as! [User]
+//        } catch {
+//            print(error.localizedDescription)
+//        }
+//        return []
+    }
+
+    public func fetchUser(_ name: String) -> User? {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+        do {
+            let users = try? contex.fetch(fetchRequest) as? [User]
+            return users?.first(where: { $0.name == name })
+        }
+    }
+
+    public func updateUser(user: User,
+                           newName: String,
+                           newPhoneNumber: String,
+                           newDateOfBirth: Date) {
+        user.name = newName
+        user.phoneNumber = newPhoneNumber
+        user.dateOfBirth = newDateOfBirth
+
+        do {
+            try contex.save()
+        } catch {
+            print(error)
+        }
+    }
+
+    public func deleteUser(user: User) {
+        contex.delete(user)
+
+        do {
+            try contex.save()
+        } catch {
+            print(error)
+        }
+    }
+
+
+}
