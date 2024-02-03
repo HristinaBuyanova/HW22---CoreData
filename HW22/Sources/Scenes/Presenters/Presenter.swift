3cb9887d4f91a5118aafd67a7e6c03c86346aed9
HristinaBuyanova <hristina-a@mail.ru>
2024-02-03 15:33:00 +0300

    Add textField

--- HW22/Sources/Scenes/Presenters/Presenter.swift
+++ HW22/Sources/Scenes/Presenters/Presenter.swift
@@ -1,8 +1,8 @@
-//
-//  Presenter.swift
-//  HW22
-//
-//  Created by Христина Буянова on 02.02.2024.
-//
 
 import Foundation
+
+class Presenter {
+    private let coreDataManager = CoreDaraManager.shared
+    
+
+}
--- HW22/Sources/Scenes/Views/MainViewController.swift
+++ HW22/Sources/Scenes/Views/MainViewController.swift
@@ -3,6 +3,25 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    var users = [User]()
+//    var presenter = Presenter?
+
+//    MARK: - Outlets
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.layer.cornerRadius = 20
+        textField.backgroundColor = .lightGray
+        textField.textColor = .systemGray
+        textField.placeholder = "Print your name here"
+        textField.textAlignment = .left
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+
+    private lazy var pressButton: UIButton = {
+        
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
